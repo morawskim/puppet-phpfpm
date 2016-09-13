e9ec56b82d34a5c8e7c659947f81f7e642c4fab7
Marcin Morawski <marcin@morawskim.pl>
2016-09-13 10:29:49 +0200

    support opensuse

--- manifests/params.pp
+++ manifests/params.pp
@@ -202,6 +202,68 @@ class phpfpm::params {
       $pool_php_admin_value           = {}
       $pool_php_admin_flag            = {}
     }
+    'suse': {
+      # Module configuration defaults
+      $poold_purge = false
+
+      # Service configuration defaults
+      $package_name                   = 'php5-fpm'
+      $service_name                   = 'php-fpm'
+      $config_dir                     = '/etc/php5/fpm'
+      $config_name                    = 'php-fpm.conf'
+      $config_template_file           = 'phpfpm/php-fpm.conf.erb'
+      $pool_dir                       = '/etc/php5/fpm'
+      $pid_file                       = '/var/run/php-fpm.pid'
+      $error_log                      = '/var/log/php-fpm.log'
+      $syslog_facility                = 'daemon'
+      $syslog_ident                   = 'php-fpm'
+      $log_level                      = 'notice'
+      $emergency_restart_threshold    = 0
+      $emergency_restart_interval     = 0
+      $process_control_timeout        = 0
+      $process_max                    = 0
+      $daemonize                      = 'yes'
+      $rlimit_files                   = undef
+      $rlimit_core                    = undef
+      $restart_command                = "systemctl reload ${service_name}.service"
+
+      # Pool configuration defaults
+      $pool_template_file             = 'phpfpm/pool.conf.erb'
+      $pool_user                      = 'nobody'
+      $pool_group                     = 'nobody'
+      $pool_listen                    = '127.0.0.1:9000'
+      $pool_listen_backlog            = undef
+      $pool_listen_owner              = undef
+      $pool_listen_group              = undef
+      $pool_listen_mode               = undef
+      $pool_listen_allowed_clients    = undef
+      $pool_pm                        = 'dynamic'
+      $pool_pm_max_children           = 5
+      $pool_pm_start_servers          = 2
+      $pool_pm_min_spare_servers      = 1
+      $pool_pm_max_spare_servers      = 3
+      $pool_pm_process_idle_timeout   = undef
+      $pool_pm_max_requests           = undef
+      $pool_pm_status_path            = undef
+      $pool_ping_path                 = undef
+      $pool_ping_response             = undef
+      $pool_access_log                = undef
+      $pool_access_format             = undef
+      $pool_slowlog                   = undef
+      $pool_req_slowlog_timeout       = undef
+      $pool_req_terminate_timeout     = undef
+      $pool_rlimit_files              = undef
+      $pool_rlimit_core               = undef
+      $pool_chroot                    = undef
+      $pool_chdir                     = undef
+      $pool_catch_workers_output      = undef
+      $pool_security_limit_extensions = undef
+      $pool_env                       = {}
+      $pool_php_value                 = {}
+      $pool_php_flag                  = {}
+      $pool_php_admin_value           = {}
+      $pool_php_admin_flag            = {}
+    }
     default: {
       fail( "Unsupported platform: ${::osfamily}" )
     }
